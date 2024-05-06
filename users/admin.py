4603bc0f920643276efc440a95678dbac7bcfd2c
KonniKall <hakon.o.gislason@gmail.com>
2024-05-06 17:34:04 +0000

    Models improved and registered to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile, Contact_Info, Employer, Job_Seeker
+    
+admin.site.register(Profile)
+admin.site.register(Contact_Info)
+admin.site.register(Employer)
+admin.site.register(Job_Seeker)
--- users/models.py
+++ users/models.py
@@ -17,13 +17,30 @@ class Contact_Info(models.Model):
 
     def __str__(self, *args, **kwargs):
         return f'{self.user.username} - {self.address}'
+    
+class Job_Seeker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='')
+    files = models.CharField(max_length=100) # Breyta seinna
+
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.bio}'
+    
+class Employer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(default='')
+    address = models.CharField(max_length=100)
+
+    cover_img = models.ImageField(default='default_cover.png')
+
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.bio}'
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     phone_nr = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     profile_img = models.ImageField(default='default_user.png')
-    
 
     time_posted = models.DateTimeField(default=timezone.now)
     def __str__(self, *args, **kwargs):
