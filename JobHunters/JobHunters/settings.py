13268b267a2462d7b3c3d8b969a5580c5bf4e3cf
KonniKall <hakon.o.gislason@gmail.com>
2024-05-06 16:52:14 +0000

    settings

--- JobHunters/JobHunters/settings.py
+++ JobHunters/JobHunters/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'JobHunters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES = { 'default': 
+             { 'ENGINE': 'django.db.backends.postgresql',
+              'NAME': 'postgres',
+              'USER': 'vln2_assignment_groups_43_user',
+              'PASSWORD': '0Rwa5LD9lY',
+              'HOST': 'verklegt-namskeid-ii.northeurope.cloudapp.azure.com',
+              'PORT': '5432', 
+              'OPTIONS': {
+                    'options': '-c search_path=vln2_assignment_groups_43'}
+             } 
+            }
 
 
 # Password validation
