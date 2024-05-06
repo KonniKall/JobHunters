1c84bfd8a02024d3f38e552df48dab7575268025
KonniKall <hakon.o.gislason@gmail.com>
2024-05-06 17:27:17 +0000

    models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Work_Experience(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workplace = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.workplace}'
+    
+class Reccomendation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_nr = models.CharField(max_length=100)
+    contact_allowed = models.BooleanField(default=True)
+    role = models.CharField(max_length=100)
+
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.workplace}'
+class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cover_letter = models.TextField(default='')
+    applied = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=100)
+
+    work_experiences = models.ManyToManyField(Work_Experience)
+    reccomendations = models.ManyToManyField(Reccomendation)
+
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.status}'
+
+    #def __str__(self, *args, **kwargs):
+    #    return f'{self.user.username} Profile'
--- users/models.py
+++ users/models.py
@@ -2,3 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Contact_Info(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.address}'
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_nr = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    profile_img = models.ImageField(default='default_user.png')
+    
+
+    time_posted = models.DateTimeField(default=timezone.now)
+    def __str__(self, *args, **kwargs):
+        return f'{self.user.username} - {self.email}'
