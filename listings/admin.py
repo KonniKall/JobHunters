397538a4f312f42548b3f3a4b872c9ee9a041235
TristanOrri <142910326+TristanOrri@users.noreply.github.com>
2024-05-07 19:06:58 +0000

    Fix models typo

--- listings/admin.py
+++ listings/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import WorkExperience, Reccomendation, Application, JobListing
-    
+from .models import WorkExperience, Recommendation, Application, JobListing
+
 admin.site.register(WorkExperience)
-admin.site.register(Reccomendation)
+admin.site.register(Recommendation)
 admin.site.register(Application)
 admin.site.register(JobListing)
--- listings/models.py
+++ listings/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class WorkExperience(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     workplace = models.CharField(max_length=100)
@@ -15,9 +16,10 @@ class WorkExperience(models.Model):
     end_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self, *args, **kwargs):
-        return f'{self.user.username} - {self.workplace}'
-    
-class Reccomendation(models.Model):
+        return f"{self.user.username} - {self.workplace}"
+
+
+class Recommendation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
@@ -26,39 +28,38 @@ class Reccomendation(models.Model):
     role = models.CharField(max_length=100)
 
     def __str__(self, *args, **kwargs):
-        return f'{self.user.username} - {self.workplace}'
-    
-
+        return f"{self.user.username} - {self.workplace}"
 
 
 class JobListing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    title = models.CharField(default='')
-    work_type = models.CharField(default='')
-    location = models.CharField(default='')
-    category = models.CharField(default='')
+    title = models.CharField(default="")
+    work_type = models.CharField(default="")
+    location = models.CharField(default="")
+    category = models.CharField(default="")
 
-    work_type = models.TextField(default='')
+    work_type = models.TextField(default="")
 
     due_date = models.DateTimeField(default=timezone.now)
     start_date = models.DateTimeField(default=timezone.now)
 
-
     def __str__(self, *args, **kwargs):
-        return f'{self.user.username} - {self.title} - {self.due_date}'
-    
-    
+        return f"{self.user.username} - {self.title} - {self.due_date}"
+
+
 class Application(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cover_letter = models.TextField(default='')
+    cover_letter = models.TextField(default="")
     applied = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=100)
 
     work_experiences = models.ManyToManyField(WorkExperience)
-    reccomendations = models.ManyToManyField(Reccomendation)
+    recommendations = models.ManyToManyField(Recommendation)
 
-    job_listing = models.ForeignKey(JobListing, on_delete=models.DO_NOTHING, default=None)
+    job_listing = models.ForeignKey(
+        JobListing, on_delete=models.DO_NOTHING, default=None
+    )
 
     def __str__(self, *args, **kwargs):
-        return f'{self.user.username} - {self.status}'
+        return f"{self.user.username} - {self.status}"
