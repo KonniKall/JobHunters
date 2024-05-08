b1b02cd6abaab8bf63d330cd9af3f2f1b135e474
KonniKall <hakon.o.gislason@gmail.com>
2024-05-08 16:52:34 +0000

    media and settings fixed

--- JobHunters/settings.py
+++ JobHunters/settings.py
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
-    'rest_framework',
-    
+    # Our apps    
     'users.apps.UsersConfig',
     'listings.apps.ListingsConfig'
 ]
