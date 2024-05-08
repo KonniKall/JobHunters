99b18769c6e7a834bd7a23708452a271af72b686
KonniKall <hakon.o.gislason@gmail.com>
2024-05-08 19:56:10 +0000

    logout implemented

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import SignInView, ProfileView
 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #path('', views.listings, name='listings-page'),
     path("sign-in/", SignInView.as_view(), name="sign-in"),
+    path('sign-out/', auth_views.LogoutView.as_view(template_name='users/sign-out.html'), name='sign-out'),
     path('profile/', ProfileView.as_view(), name='profile')
 ]
