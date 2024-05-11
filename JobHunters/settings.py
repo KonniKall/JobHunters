4470270f8bee91ae0bb964865dd892f0bcbde5c0
KonniKall <hakon.o.gislason@gmail.com>
2024-05-11 20:38:21 +0000

    processor fix

--- JobHunters/settings.py
+++ JobHunters/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                #'users.utils.context_processors.user_type'
+                'users.utils.context_processors.user_type'
             ],
         },
     },
--- users/utils/context_processors.py
+++ users/utils/context_processors.py
@@ -4,12 +4,10 @@ from django.contrib.auth.decorators import login_required
 
 from django.template import Context
 
-@login_required
 def user_type(request):
     if request.user.is_authenticated:
         user_type = JobSeeker.objects.filter(user=request.user).first()
         if user_type == None:
             user_type = Employer.objects.filter(user=request.user).first()
-        user_type = 'dasadasda'
         return {'user_type': user_type}
-    return {'asdas': 'adsasda'}
+    return {}
