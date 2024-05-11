0f7094f10512ab5818b23d41d64c0a8ebf5bf2b7
KonniKall <hakon.o.gislason@gmail.com>
2024-05-11 20:33:13 +0000

    quickfix

--- users/utils/context_processors.py
+++ users/utils/context_processors.py
@@ -2,9 +2,14 @@ from django.contrib.auth.models import User
 from ..models import JobSeeker, Employer
 from django.contrib.auth.decorators import login_required
 
+from django.template import Context
+
 @login_required
-def user_type(request):  
-    user_type = JobSeeker.objects.filter(user=request.user).first()
-    if user_type == None:
-        user_type = Employer.objects.filter(user=request.user).first()
-    return {'user_type': user_type}
+def user_type(request):
+    if request.user.is_authenticated:
+        user_type = JobSeeker.objects.filter(user=request.user).first()
+        if user_type == None:
+            user_type = Employer.objects.filter(user=request.user).first()
+        user_type = 'dasadasda'
+        return {'user_type': user_type}
+    return {'asdas': 'adsasda'}
