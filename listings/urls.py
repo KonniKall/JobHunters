757cc9a4f1c11fb69e4bfadde5a0e49f549534f8
KonniKall <hakon.o.gislason@gmail.com>
2024-05-14 16:52:27 +0000

    single job_listing added

--- listings/urls.py
+++ listings/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import ListingsView
+from .views import ListingsView, JobListingView
 
 from django.conf.urls import handler404
 
 urlpatterns = [
     #path('', views.listings, name='listings-page'),
-    path('', ListingsView.as_view(), name='index')
-    
+    path('', ListingsView.as_view(), name='index'),
+    path('listing/<str:listing>/', JobListingView.as_view(), name='listing')
 ]
--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,7 @@ from django.views.generic import View
 from django.http import JsonResponse
 
 from .models import JobListing
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, redirect
 
 from is_ajax import is_ajax
 
@@ -29,3 +29,13 @@ class ListingsView(View):
         # filter.delete()
 
         return JsonResponse({"result": "ok"}, status=200)
+
+
+class JobListingView(View):
+    def get(self, request, listing):
+        listing = JobListing.objects.filter(pk=listing).first()
+        if listing == None:
+            # Appendar við URL-in sem þarf að laga
+            return redirect('/users.views.custom_page_not_found_view')
+        context = {'listing': listing}
+        return render(request, "listings/job_listing.html", context)
