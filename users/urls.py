4862b287090c8740ab024cb29ae6917a874dc7ea
TristanOrri <142910326+TristanOrri@users.noreply.github.com>
2024-05-16 19:15:04 +0000

    add profile urls

--- users/urls.py
+++ users/urls.py
@@ -1,26 +1,43 @@
 from django.urls import path
 
-from .views import SignInView, ProfileView, ApplicationsView, JobListingsView, JobListingView, WorkplacesView, WorkplaceView
+from .views import (
+    SignInView,
+    ProfileView,
+    EditProfileView,
+    ApplicationsView,
+    JobListingsView,
+    JobListingView,
+    WorkplacesView,
+    WorkplaceView,
+)
 
 from listings.views import ApplicationView
 
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #path('', views.listings, name='listings-page'),
+    # path('', views.listings, name='listings-page'),
     path("sign-in/", SignInView.as_view(), name="sign-in"),
-    path('sign-out/', auth_views.LogoutView.as_view(template_name='users/sign-out.html'), name='sign-out'),
-
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('profile/applications/', ApplicationsView.as_view(), name='sent-applications'),
-    path('profile/applications/<str:application>/', ApplicationView.as_view(), name='sent-applications'),
-
-    path('profile/job-listings/', JobListingsView.as_view(), name='my-job-listings'),
-    path('profile/job-listings/<str:job_listing>/', JobListingView.as_view(), name='my-job-listings'),
-
-    path('workplaces/', WorkplacesView.as_view(), name='workplaces'),
-    path('workplaces/<str:workplace>/', WorkplaceView.as_view(), name='workplaces'),
-
+    path(
+        "sign-out/",
+        auth_views.LogoutView.as_view(template_name="users/sign-out.html"),
+        name="sign-out",
+    ),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/edit", EditProfileView.as_view(), name="edit-profile"),
+    path("profile/applications/", ApplicationsView.as_view(), name="sent-applications"),
+    path(
+        "profile/applications/<str:application>/",
+        ApplicationView.as_view(),
+        name="sent-applications",
+    ),
+    path("profile/job-listings/", JobListingsView.as_view(), name="my-job-listings"),
+    path(
+        "profile/job-listings/<str:job_listing>/",
+        JobListingView.as_view(),
+        name="my-job-listings",
+    ),
+    path("workplaces/", WorkplacesView.as_view(), name="workplaces"),
+    path("workplaces/<str:workplace>/", WorkplaceView.as_view(), name="workplaces"),
     # path('workplaces/<int:id>/', WorkplaceView.as_view(), name='workplace-detail'),
-
 ]
