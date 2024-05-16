d85c47cbc9f5cfeb5fc2e91dda50f235120e7b17
TristanOrri <142910326+TristanOrri@users.noreply.github.com>
2024-05-16 19:15:43 +0000

    add profile views

--- users/views.py
+++ users/views.py
@@ -9,14 +9,16 @@ from is_ajax import is_ajax
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 
-from .forms import UserSignInForm #, UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import (
+    UserSignInForm,
+)  # , UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.http import HttpResponse
 
 import json
 
 from listings.models import Application, JobListing
-from .models import Employer, JobSeeker
+from .models import Employer, JobSeeker, ContactInfo, Profile
 from django.contrib.auth.models import User
 
 from django.shortcuts import redirect
@@ -39,32 +41,37 @@ class SignInView(View):
     def post(self, request):
         form = UserSignInForm(data=request.POST)
         if form.is_valid():
-            print('working?')
-            usernameS = form.cleaned_data.get('username')
-            passwordS = form.cleaned_data.get('password')
-            messages.success(request, f'Signed in as {usernameS}.')
+            print("working?")
+            usernameS = form.cleaned_data.get("username")
+            passwordS = form.cleaned_data.get("password")
+            messages.success(request, f"Signed in as {usernameS}.")
             user = authenticate(username=usernameS, password=passwordS)
             auth_login(request, user)
-            print('authenticated')
+            print("authenticated")
             if is_ajax(request=request):
                 print("AJAX2")
 
-            return HttpResponse(json.dumps({'message': '200'}))
+            return HttpResponse(json.dumps({"message": "200"}))
         else:
             if is_ajax(request=request):
                 print(form.error_messages)
-                print(form.error_messages['invalid_login'])
+                print(form.error_messages["invalid_login"])
                 print("AJAX")
 
-        return HttpResponse(json.dumps({'message': "Incorrect username or password. Try again."}))
+        return HttpResponse(
+            json.dumps({"message": "Incorrect username or password. Try again."})
+        )
+
 
 class ProfileView(View):
 
     def get(self, request):
+        contact_info = ContactInfo.objects.filter(user=request.user).first()
+        profile = Profile.objects.filter(user=request.user).first()
         if is_ajax(request=request):
             print(f"working2")
 
-        context = {}
+        context = {"contact_info": contact_info, "profile": profile}
         return render(request, "users/profile.html", context)
 
     def post(self, request, name):
@@ -72,87 +79,101 @@ class ProfileView(View):
         return JsonResponse({"result": "ok"}, status=200)
 
 
+class EditProfileView(View):
+    def get(self, request):
+        contact_info = ContactInfo.objects.filter(user=request.user).first()
+        profile = Profile.objects.filter(user=request.user).first()
+        if is_ajax(request=request):
+            print(f"working2")
+
+        context = {"contact_info": contact_info, "profile": profile}
+        return render(request, "users/edit-profile.html", context)
+
+    def post(self, request):
+        # form = ProfileEditForm(data=request.POST)
+        return JsonResponse({"result": "ok"}, status=200)
+
+
 class ApplicationsView(View):
 
     def get(self, request):
-        #Job seeker check
+        # Job seeker check
         if JobSeeker.objects.filter(user=request.user).first() == None:
-            return redirect('/users.views.custom_page_not_found_view')
+            return redirect("/users.views.custom_page_not_found_view")
         applications = Application.objects.filter(user=request.user)
-        context = {'applications': applications}
+        context = {"applications": applications}
         return render(request, "users/applications.html", context)
 
     def post(self, request, name):
 
         return JsonResponse({"result": "ok"}, status=200)
-    
-
 
 
 class JobListingsView(View):
 
     def get(self, request):
-        #Employer check
+        # Employer check
         if Employer.objects.filter(user=request.user).first() == None:
-            return redirect('/users.views.custom_page_not_found_view')
-        
+            return redirect("/users.views.custom_page_not_found_view")
+
         job_listings = JobListing.objects.filter(user=request.user)
-        context = {'job_listings': job_listings}
+        context = {"job_listings": job_listings}
         return render(request, "users/job-listings.html", context)
 
     def post(self, request, name):
 
         return JsonResponse({"result": "ok"}, status=200)
-    
+
+
 class JobListingView(View):
 
     def get(self, request, job_listing):
-        #Employer check
+        # Employer check
         if Employer.objects.filter(user=request.user).first() == None:
-            return redirect('/users.views.custom_page_not_found_view')
+            return redirect("/users.views.custom_page_not_found_view")
 
-        #Nota mögulega ehv annað en pk seinna
-        job_listing = JobListing.objects.filter(user=request.user, pk=job_listing).first()
+        # Nota mögulega ehv annað en pk seinna
+        job_listing = JobListing.objects.filter(
+            user=request.user, pk=job_listing
+        ).first()
         if job_listing == None:
             # Appendar við URL-in sem þarf að laga
-            return redirect('/users.views.custom_page_not_found_view')
-        context = {'job_listing': job_listing}
+            return redirect("/users.views.custom_page_not_found_view")
+        context = {"job_listing": job_listing}
         return render(request, "users/job-listing.html", context)
 
     def post(self, request, name):
 
         return JsonResponse({"result": "ok"}, status=200)
-    
+
+
 class WorkplacesView(View):
 
     def get(self, request):
         workplaces = Employer.objects.all()
-        context = {'workplaces': workplaces}
+        context = {"workplaces": workplaces}
         return render(request, "users/workplaces.html", context)
 
     def post(self, request, name):
 
         return JsonResponse({"result": "ok"}, status=200)
-    
+
+
 class WorkplaceView(View):
 
     def get(self, request, workplace):
         workplace = Employer.objects.filter(user=request.user, pk=workplace).first()
         if workplace == None:
             # Appendar við URL-in sem þarf að laga
-            return redirect('/users.views.custom_page_not_found_view')
-        context = {'workplace': workplace}
+            return redirect("/users.views.custom_page_not_found_view")
+        context = {"workplace": workplace}
         return render(request, "users/workplace.html", context)
 
     def post(self, request, name):
 
         return JsonResponse({"result": "ok"}, status=200)
-    
-
 
 
 def custom_page_not_found_view(request, exception=None):
 
     return render(request, "listings/404.html")
-
-
