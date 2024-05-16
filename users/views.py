c9f9f5874d1c37a712ed6ad87483a0673c662dbd
KonniKall <hakon.o.gislason@gmail.com>
2024-05-16 16:04:05 +0000

    profile changes

--- users/views.py
+++ users/views.py
@@ -9,17 +9,18 @@ from is_ajax import is_ajax
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 
-from .forms import UserSignInForm #, UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserSignInForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.http import HttpResponse
 
 import json
 
 from listings.models import Application, JobListing
-from .models import Employer, JobSeeker
+from .models import Employer, JobSeeker, Profile
 from django.contrib.auth.models import User
 
 from django.shortcuts import redirect
+
 
 # Create your views here.
 
@@ -64,10 +65,24 @@ class ProfileView(View):
         if is_ajax(request=request):
             print(f"working2")
 
-        context = {}
+        u_form = UserUpdateForm(instance=request.user)
+
+        profile = Profile.objects.filter(user=request.user).first()
+        p_form = ProfileUpdateForm(instance=profile)
+
+        profile_additional = Employer.objects.filter(user=request.user).first()
+        if profile_additional == None:
+            profile_additional = JobSeeker.objects.filter(user=request.user).first()
+        context = {
+            'u_form': u_form,
+            'p_form': p_form,
+
+            'profile': profile,
+            'profile_additional': profile_additional
+            }
         return render(request, "users/profile.html", context)
 
-    def post(self, request, name):
+    def post(self, request):
 
         return JsonResponse({"result": "ok"}, status=200)
 
